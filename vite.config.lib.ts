858967a8ad54d34b9b3c8006952bec83759cfa9a
fxi <f@fxi.io>
2026-03-12 11:14:50 +0100

    feat: npm package config

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [glsl(), dts({ include: ["src/lib"], rollupTypes: true })],
   build: {
     lib: {
       entry: "src/lib/index.ts",
